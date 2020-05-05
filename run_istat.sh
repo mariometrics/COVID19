897120c8fb5ebc18b1349bad45d5036b62e05508
mariometrics <marchetti224@gmail.com>
2020-05-05 13:08:06 +0200

    Mod run

--- run_istat.sh
+++ run_istat.sh
@@ -4,3 +4,17 @@ set -e
 DIR=$(dirname $(realpath "$0"))
 cd $DIR/src
 python3 src_deceases_istat.py
+echo ""
+
+echo "ISTAT data analysis finished"
+echo "Start Rt analysis."
+echo ""
+
+cd ../RealtimeR0_Italy 
+#python3 ReatimeR0_Italy.py
+echo ""
+echo "Open two summry plot"
+cd .. 
+xdg-open ./RealtimeR0_Italy/rt_Italy.png
+xdg-opne ./plot/covid_italy.png
+echo "ALL DONE!"
